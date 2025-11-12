9a6cf5a3006371221651d1c6afde4de20b1cd3d0
Haim Aflalo <kodkodhaim@gmail.com>
2025-11-12 15:08:17 +0200

    get all items

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+import uvicorn
 import json
 
 app = FastAPI()
@@ -15,4 +16,11 @@ def save_data(data):
     with open("data.json", "x") as json_file:
         json.dump(data,json_file)
 
+
+@app.get("/items")
+def get_all_items():
+    return load_data()
+
+# if __name__ == "__main__":
+#     uvicorn.run(app,host="localhost",port=8000)
 
