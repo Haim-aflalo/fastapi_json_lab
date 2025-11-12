df924a08a3a13ecc75c36be1eef53f04b9c99a31
Haim Aflalo <kodkodhaim@gmail.com>
2025-11-12 17:57:24 +0200

    exercise finish

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI, HTTPException
 import uvicorn
 import json
+from pydantic import BaseModel
 
 app = FastAPI()
-
+class Item(BaseModel):
+    id:int
+    name:str
+    price:float
 
 def load_data():
     with open("data.json") as json_file:
@@ -11,16 +15,25 @@ def load_data():
         return data
 
 
-
 def save_data(data):
-    with open("data.json", "x") as json_file:
-        json.dump(data,json_file)
+    with open("data.json", "w") as json_file:
+        json.dump(data,json_file, indent=4)
 
 
 @app.get("/items")
 def get_all_items():
     return load_data()
 
-# if __name__ == "__main__":
-#     uvicorn.run(app,host="localhost",port=8000)
+
+@app.put("/items")
+def update_price(it:Item):
+    items = load_data()
+    for item in items:
+        if item["id"] == it.id:
+            item["price"]  = it.price
+        save_data(items)
+    return  "hello"
+
+if __name__ == "__main__":
+    uvicorn.run(app,host="localhost",port=8000)
 
